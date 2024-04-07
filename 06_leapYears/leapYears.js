071c0b34a2d17e627d9c6db5ce92efe194112b89
brianpocasangre <brianpocasangre1@gmail.com>
2024-04-06 19:28:19 -0700

    complete leapYear challenge if statements work up to century test

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,16 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  if (year % 4 === 0) {
+    if (year % 100 !== 0 || year % 400 === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    return false;
+  }
 };
+// broke down the solution in chatgpt as i was writting out
+// if statements and got stuck on the last errors
 
 // Do not edit below this line
 module.exports = leapYears;
