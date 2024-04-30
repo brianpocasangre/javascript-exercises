fc7a6b870c652adfd852b68091708bc8ec47faac
brianpocasangre <brianpocasangre1@gmail.com>
2024-04-30 14:30:08 -0700

    work on if/else statements and loop to get fibonacci sequence and result

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  if (num == 0) {
+    return 0;
+  } else if (num == 1) {
+    return 1;
+  } else if (num < 0) {
+    return 'OOPS';
+  }
+  let fib = [0, 1];
+  for (i = 2; i <= num; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+  }
+  return fib[num];
 };
 
 // Do not edit below this line
