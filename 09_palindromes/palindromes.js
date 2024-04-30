7388b014db079d8db52314503b4d9ccf0e2b1f3d
brianpocasangre <brianpocasangre1@gmail.com>
2024-04-30 10:21:27 -0700

    struggle with array methods

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const alphaNumeric = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+  const newString = str
+    .toLowerCase()
+    .split('')
+    .filter(character => alphaNumeric.includes(character))
+    .join('');
+
+  const reversedString = newString.split('').reverse().join('');
+
+  return newString === reversedString;
 };
+// was not able to complete challenge ---- lesson learned array methods on strings as they operate similar to an array of characters.
 
 // Do not edit below this line
 module.exports = palindromes;
