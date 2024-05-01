e0c12350750cbaf66db8fa9383f0d0908895e70b
brianpocasangre <brianpocasangre1@gmail.com>
2024-05-01 14:57:07 -0700

    use sort and return first option / reduce a better option

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  let date = new Date().getFullYear();
+  const oldest = people.sort(function (a, b) {
+    if (a.yearOfDeath === undefined) {
+      a.yearOfDeath = date;
+    } else if (b.yearOfDeath === undefined) {
+      b.yearOfDeath = date;
+    }
+    const lastPerson = a.yearOfDeath - a.yearOfBirth;
+    const nextPerson = b.yearOfDeath - b.yearOfBirth;
+    return lastPerson > nextPerson ? -1 : 1;
+  });
+  return oldest[0];
 };
 
 // Do not edit below this line
